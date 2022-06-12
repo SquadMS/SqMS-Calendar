0e6dda366ae2b9427b67adc3b2cf713ddc3af859
Patrick <privat@skyraptor.eu>
2022-06-12 14:05:50 +0200

    Create 2022_06_12_140000_create_events_table.php

---
+++ database/migrations/2022_06_12_140000_create_events_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->bigIncrements('id');
+          
+            $table->string('google_calendar_id')->nullable();
+
+            $table->text('title');
+            $table->text('description');
+
+            $table->boolean('all_day')->default(true);
+            $table->dateTime('start');
+            $table->dateTime('end');
+
+            $table->string('type');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
