0b6012428056735ae0045bd1c3a037f6482654f4
Patrick <privat@skyraptor.eu>
2022-06-12 14:23:20 +0200

    Create EventType.php

---
+++ src/Models/EventType.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace SquadMS\Calender\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EventType extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];   
+}
