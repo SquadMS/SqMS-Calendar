41a8bf38ac213e532f5de3d9838fd1b6fe85e2f1
Patrick <privat@skyraptor.eu>
2022-06-12 14:24:31 +0200

    Create EventTypeResource.php

---
+++ src/Filament/Resources/EventTypeResource.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace SquadMS\Calendar\Filament\Resources;
+
+use SquadMS\Calendar\Filament\Resources\EventResource\Pages;
+use SquadMS\Calendar\Models\EventType;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Resources\Concerns\Translatable;
+
+class EventTypeResource extends Resource
+{
+    use Translatable;
+    
+    protected static ?string $navigationGroup = 'Calendar Management';
+
+    protected static ?string $model = EventType::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->rules('required|string|min:1|max:255')
+                    ->required()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->sortable(),
+            ])
+            ->filters([
+                //
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListEventType::route('/'),
+            'create' => Pages\CreateEventType::route('/create'),
+            'edit' => Pages\EditEventType::route('/{record}/edit'),
+        ];
+    }
+}
