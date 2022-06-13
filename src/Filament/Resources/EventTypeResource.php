3c3a14cd032c9f62846bda4d27cb26d4a81098e0
Patrick <privat@skyraptor.eu>
2022-06-13 02:38:47 +0200

    Update EventTypeResource.php

--- src/Filament/Resources/EventTypeResource.php
+++ src/Filament/Resources/EventTypeResource.php
@@ -9,7 +9,7 @@ use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
-use Filament\Resources\Concerns\Translatable;
+use SquadMS\Foundation\Resources\Concerns\Translatable;
 
 class EventTypeResource extends Resource
 {
